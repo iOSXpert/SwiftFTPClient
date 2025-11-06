13082df41496f49ff0aac6b6fd29e7f95716cfd3
Daniel Nauerz <Daniel.Nauerz@iosxpert.biz>
2025-11-06 09:43:32 +0100

    no new line in error messages

--- Sources/SwiftFTPClient/FTPError.swift
+++ Sources/SwiftFTPClient/FTPError.swift
@@ -18,15 +18,22 @@ public enum FTPError: Error {
     public var localizedDescription: String {
         switch self {
         case .connectionFailed(let message):
-            return "Connection Failed: \(message)"
+            return "Connection Failed: \(message.cleanedFTPErrorMessage)"
         case .authenticationFailed(let message):
-            return "Authentication Failed: \(message)"
+            return "Authentication Failed: \(message.cleanedFTPErrorMessage)"
         case .transferFailed(let message):
-            return "Transfer Failed: \(message)"
+            return "Transfer Failed: \(message.cleanedFTPErrorMessage)"
         case .cancelled:
             return "Operation Cancelled"
         case .other(let message):
-            return "Error: \(message)"
+            return "Error: \(message.cleanedFTPErrorMessage)"
         }
+    }
+}
+
+extension String {
+    var cleanedFTPErrorMessage: String {
+        return self.replacingOccurrences(of: "\n", with: "")
+            .replacingOccurrences(of: "\r", with: "")
     }
 }
